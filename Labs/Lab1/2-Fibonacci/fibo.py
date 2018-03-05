69003ffa485f13c7879b1b7ddbc7b67652e1d8df
jhriverasa <jhriverasa@unal.edu.co>
2018-03-04 20:28:19 -0500

    Update fibo.py

--- Labs/Lab1/2-Fibonacci/fibo.py
+++ Labs/Lab1/2-Fibonacci/fibo.py
@@ -1,10 +1,8 @@
-
 import sys
 #Create variables and assign their initial values.
 IntFn_2=0
 IntFn_1=1
 IntFn=0
-#try to compute 100-th term of fibonacci secuence .
 n= 100
 overFlow=0
 for i in range(n):
@@ -12,7 +10,9 @@ for i in range(n):
     IntFn_2=IntFn_1
     IntFn_1=IntFn
     IntFn=IntFn_2+IntFn_1
-    #--------   
+    #--------------------
     if(IntFn>sys.maxint and overFlow==0):
-        print ("Overflow computing "+str(i)+"-th term of Fibonacci Sequence(data type: Int) ");
+        print ("Overflow computing "+str(i+1)+"-th term of Fibonacci Sequence(data type: Int) ");
+        print(str(i)+"-th term is: "+str(IntFn_1)+" and the next one is: "+str(IntFn))
         overFlow=1
+        print ("Max int: "+str(sys.maxint))
